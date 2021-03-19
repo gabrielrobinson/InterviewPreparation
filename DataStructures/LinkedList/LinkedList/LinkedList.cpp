e79ac520581a7cff6316881ffa98a14eb549f928
Gabriel Robinson <gabrielrobinson27@protonmail.com>
2021-03-19 17:33:33 -0600

    edited .gitignore

---
+++ DataStructures/LinkedList/LinkedList/LinkedList.cpp
@@ -0,0 +1,8 @@
+//
+//  LinkedList.cpp
+//  LinkedList
+//
+//  Created by Gabriel Robinson on 3/19/21.
+//
+
+#include <stdio.h>
---
+++ DataStructures/LinkedList/LinkedList/LinkedList.h
@@ -0,0 +1,12 @@
+//
+//  LinkedList.h
+//  LinkedList
+//
+//  Created by Gabriel Robinson on 3/19/21.
+//
+
+#ifndef LinkedList_h
+#define LinkedList_h
+
+
+#endif /* LinkedList_h */
