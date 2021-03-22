5041d14e74bde807702a74f765f03f245c68bc64
Gabriel Robinson <gabrielrobinson27@protonmail.com>
2021-03-22 16:24:58 -0600

    Linked list WIP

--- DataStructures/LinkedList/LinkedList/LinkedList.cpp
+++ DataStructures/LinkedList/LinkedList/LinkedList.cpp
@@ -4,5 +4,32 @@
 //
 //  Created by Gabriel Robinson on 3/19/21.
 //
-
+#include "LinkedList.h"
 #include <stdio.h>
+
+LinkedList::LinkedList() { }
+
+LinkedList::~LinkedList() {
+    
+}
+
+void LinkedList::pushBack(int value) {
+    if (this->headNode == NULL) {
+        this->headNode = new LinkedListNode();
+        this->headNode->value = value;
+    } else {
+        LinkedListNode* current = this->headNode;
+        while(current->next != NULL) current = current->next;
+        current->next = new LinkedListNode();
+        current->next->value = value;
+    }
+    
+}
+
+void LinkedList::remove(int value) {
+    
+}
+
+void LinkedList::contains(int value) {
+    
+}
